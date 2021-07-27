c825e7e0a75b1bdee3b1dd20aa2d05ce1d99e870
Kat Huang <khuang129@gmail.com>
2021-07-26 21:38:50 -0700

    initialized Leaflet map to display

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
 import "./App.css";
-
+import Map from "./Map";
 function App() {
-  return (
-    <div className="App">
-      <h1>Hello!</h1>
-    </div>
-  );
+  return <Map />;
 }
 
 export default App;
